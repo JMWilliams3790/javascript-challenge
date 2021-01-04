2687428632944bad32941dcb2b71536aca6db0af
Josh <JMWilliams3790@gmail.com>
2021-01-03 18:13:39 -0800

    fixing index.html

--- UFO-level-1/app.js
+++ UFO-level-1/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach(function(UFOReport) {
+  console.log(UFOReport);
+  var row = tbody.append("tr");
+  Object.entries(UFOReport).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
